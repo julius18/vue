87c62c6d0ec61dc258ed63ec8e0c5fe86b2a2a80
Juliusman <juliusman.sine@karmagroup.com>
2018-11-30 15:38:53 +0800

    update

--- js/app.js
+++ js/app.js
@@ -3,32 +3,68 @@ new Vue ({
     data: {
         playerHealth: 100,
         monsterHealth: 100,
-        gameIsRunning: false
+        gameIsRunning: false,
+        turns: []
     },
     methods: {
         startGame: function () {
             this.gameIsRunning = true;
             this.playerHealth = 100;
             this.monsterHealth = 100;
+            this.turns = [];
         },
         attack: function () {
 
-            this.monsterHealth -= this.calculateDamage(3, 10);
-
+            var damage = this.calculateDamage(3, 10);
+            this.monsterHealth -= damage;
+            this.turns.unshift({
+                isPlayer: true,
+                text: 'Payer hits Monster for ' + damage
+            });
             if(this.checkWin()){
                 return;
             }
-            this.playerHealth -= this.calculateDamage(5, 12);
-            this.checkWin();
+
+            this.monsterAttacks();
         },
         specialAttack: function () {
-
+            var damage = this.calculateDamage(10, 20)
+            this.monsterHealth -= damage;
+            this.turns.unshift({
+                isPlayer: true,
+                text: 'Payer hits Monster hard for ' + damage
+            });
+            if (this.checkWin()) {
+                return;
+            }
+            
+            this.monsterAttacks();
         },
         heal: function () {
-
+            if (this.playerHealth <= 90){
+                this.playerHealth += 10;
+            } else {
+                this.playerHealth = 100;
+            }
+            this.turns.unshift({
+                isPlayer: true,
+                text: 'Payer heals Monster for 10'
+            });
+            this.calculateDamage(10, 20)
+            this.monsterAttacks();
         },
         giveUp: function (){
+            this.gameIsRunning = false;
 
+        },
+        monsterAttacks: function(){
+            var damager = this.calculateDamage(5, 12);
+            this.playerHealth -= damage;
+            this.checkWin();
+            this.turns.unshift({
+                isPlayer: false,
+                text: 'Monster hits Player for ' + damage
+            });
         },
         calculateDamage: function(min, max){
             return Math.max(Math.floor(Math.random() * max) + 1, min);
